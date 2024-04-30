1600a698369afbd5acca38e53f56f3320d3a4359
Stéphane Dai <127096387+sdai02@users.noreply.github.com>
2024-04-30 09:53:10 +0200

    intialisation the project

--- src/App.js
+++ src/App.js
@@ -1,50 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-import Input from './Input';
-import CountButton from './CountButton';
-import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomePage from './HomePage';
-import ToDoList from './ToDoList';
-import Compteur from './Compteur';
+import Home from './Home';
+import Page from './Pages/Page';
 
-
-const App = () => {
+function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="HomePage" element={<HomePage/>}/>
-        <Route path="ToDoList" element={<ToDoList/>}/>
-        <Route path="Compteur" element={<Compteur/>}/>
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='page' element={<Page/>}/>
+          </Routes>
+        </BrowserRouter>
+    </div>
   );
 }
-
-// function App() {
-//   const [variable, setVariable] = useState('string')
-//   const variableButton = () => {
-//     if (variable === 'variable') {
-//       return (
-//         <button>
-//           'ceci est un bouton'
-//         </button>
-//     )
-//   }
-//   return (
-//     <p>ceci n'est pas un bouton</p>
-//   )
-// }
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         {variableButton()}
-//         <Input text={'Yayyyyy'}/>
-//         <CountButton text={'aaaaahaha'}/>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
