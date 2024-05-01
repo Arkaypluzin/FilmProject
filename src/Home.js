8b2e471f4c30bed2511cd1ffee0b590cccbd4659
Stéphane Dai <127096387+sdai02@users.noreply.github.com>
2024-05-01 13:38:05 +0200

    update file

--- src/Home.js
+++ src/Home.js
@@ -2,25 +2,37 @@ import React, {useState, useEffect} from "react";
 
 
 
+
 const Home = () => {
-    const [userData, setUserdata] = useState('')
+
+  
+    const [userData, setUserData] = useState('')
+    
     const url = "https://api.themoviedb.org/3/movie/157336?api_key=9fdb6f1a8bdb08f927d4a129f067516a"
     const getData = async (url) => {
     const rawData = await fetch(url)
     const jsonData = await rawData.json()
-    setUserdata(jsonData)
+    setUserData(jsonData)
   }
 useEffect(()=>{
     getData(url)
   },[])
 
+
     return(
         <div>
             <p>home page</p>
-            {userData.original_title}
-            {userData.genres}
+            
+            
+            {/* Là je défini si userData.genres est défini ou non nul, s'il existe le .map est enclencher pour faire la 
+            boucle en affichent tout le contenue*/}
+           
+            {userData.genres && userData.genres.map((item,index) => <li key={index}> {item.name} </li>)}
+            
         </div>
     )
+
+    
 }
 
 export default Home;
