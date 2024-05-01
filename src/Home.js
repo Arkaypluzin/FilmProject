19790509b414a23eaedbea88efcb36fb5c081b7e
Stéphane Dai <127096387+sdai02@users.noreply.github.com>
2024-05-01 15:01:16 +0200

    save

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
   
     const [userData, setUserData] = useState('')
     
-    const url = "https://api.themoviedb.org/3/movie/157336?api_key=9fdb6f1a8bdb08f927d4a129f067516a"
+    const url = "https://api.themoviedb.org/3/movie/popular?api_key=9fdb6f1a8bdb08f927d4a129f067516a"
     const getData = async (url) => {
     const rawData = await fetch(url)
     const jsonData = await rawData.json()
@@ -21,13 +21,14 @@ useEffect(()=>{
 
     return(
         <div>
-            <p>home page</p>
-            
-            
-            {/* Là je défini si userData.genres est défini ou non nul, s'il existe le .map est enclencher pour faire la 
-            boucle en affichent tout le contenue*/}
+          <p>home page</p>
+          
+          
+          {/* Là je défini si userData.genres est défini ou non nul, s'il existe le .map est enclencher pour faire la 
+          boucle en affichent tout le contenue*/}
+          <p> {userData.original_title} </p>
            
-            {userData.genres && userData.genres.map((item,index) => <li key={index}> {item.name} </li>)}
+          {userData.original_title && userData.original_title.map((item,index) =>  (<img key={index} src= {`https://image.tmdb.org/t/p/w500/${item}`} alt=""/>))}
             
         </div>
     )
