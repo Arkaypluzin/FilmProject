c29c40df72e4ebc8ec8c470883d228f6f120a774
Arkaypluzin <michael647@live.fr>
2024-05-02 13:33:16 +0200

    animation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './HomePage';
 import FilmList from './FilmList';
-
+import LoadingScreen from './LoadingScreen'; // Importez le composant LoadingScreen
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="HomePage" element={<HomePage/>}/>
-        <Route path="FilmList" element={<FilmList/>}/>
-      </Routes>
+      {loading ? (
+        <LoadingScreen />
+      ) : (
+        <Routes>
+          <Route path="HomePage" element={<HomePage />} />
+          <Route path="FilmList" element={<FilmList />} />
+        </Routes>
+      )}
     </BrowserRouter>
   );
 }
+
 export default App;
