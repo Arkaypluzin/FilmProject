80e484038dbea15c75d557ee9a01ef6190ebe5c4
Stéphane Dai <127096387+sdai02@users.noreply.github.com>
2024-05-02 13:48:02 +0200

    add css

--- src/App.js
+++ src/App.js
@@ -3,16 +3,33 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Page from './Pages/Page';
+import LoadingScreen from './Components/LoadingScreen';
+import { useEffect, useState } from 'react';
+
 
 
 function App() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+
   return (
     <div className="App">
       <BrowserRouter>
+      {loading ? (
+        <LoadingScreen />
+      ) : (
           <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='page/:id' element={<Page/>}/>
           </Routes>
+          )}
         </BrowserRouter>
     </div>
   );
